5d4caa1df493d37b2def747d27653bb4726cbb63
suraj-khanal <113327166+suraj-khanal@users.noreply.github.com>
2023-10-24 18:25:47 -0400

    First Commit

---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Simple Webpage</title>
+</head>
+<body>
+    <h1>Welcome to My Webpage - JSP</h1>
+    <p>This is a simple HTML webpage.</p>
+</body>
+</html>
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE )
+values (101,'suraj', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE )
+values (102,'suraj', 'Get Azure Certifiede', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE )
+values (103,'suraj', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE )
+values (104,'suraj', 'Learn DevOps', CURRENT_DATE(), false);
